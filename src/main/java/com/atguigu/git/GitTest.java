32b0dfa19a52e0da21e0330bdccb85f51fb95672
XR <1091605938@qq.com>
2021-07-12 15:36:16 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author xR
+ * @create 2021-07-12 15:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hell git!");
+    }
+}
